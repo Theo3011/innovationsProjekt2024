7565f1fff6df7a5e37ac3fdbeb1f417ff52f13b1
CCOvergaard <118440598+CCOvergaard@users.noreply.github.com>
2024-11-07 13:19:25 +0100

    u
    u

--- innovationsProjekt/App.js
+++ innovationsProjekt/App.js
@@ -16,3 +16,20 @@ import CreateOfferPage from ".components/createOfferPage";
 import Settings from "./components/settings";
 
 
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TutorSignUp" component={TutorSignUp} />
+        <Stack.Screen name="StudentSearch" component={StudentSearch} />
+        <Stack.Screen name="CameraTest" component={CameraTest} />
+        <Stack.Screen name="ImageScreen" component={ImageScreen} />
+        <Stack.Screen name="TutorProfile" component={TutorProfile} />
+        <Stack.Screen name="TutorDetails" component={TutorDetails} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
