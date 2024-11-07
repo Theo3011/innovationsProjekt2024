76b3a86cf34921b50923f857fdcaece8f9a12243
Theo3011 <44906882+Theo3011@users.noreply.github.com>
2024-11-07 12:53:38 +0100

    s
    s

--- innovationsProjekt/App.js
+++ innovationsProjekt/App.js
@@ -6,11 +6,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import Settings from "./components/settings";
 
 // Importer skærme (fra den nye filstruktur)
 import DashBoard from "./components/dashBoard";
 import OfferPage from "./components/offerPage";
 import ChatPage from "./components/chatPage";
 import LoginCreatePage from "./components/loginCreatePage";
-import CreatePage from ".components/createpage";
+import CreateOfferPage from ".components/createOfferPage";
+import Settings from "./components/settings";
