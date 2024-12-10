c5c2a8006a1c4e8f74b37fa3f3f8be1d4f76d118
Theo3011 <44906882+Theo3011@users.noreply.github.com>
2024-12-10 10:17:12 +0100

    s
    s

--- innovationsProjekt/components/chatPage.js
+++ innovationsProjekt/components/chatPage.js
@@ -77,9 +77,11 @@ const ChatPage = () => {
             onPress={() => handleChatPress(item.id, item.name)}
           >
             <View style={styles.chatDetails}>
-              <Text style={styles.chatAuthor}>Bruger: {item.author}</Text>
+              <Text style={styles.chatAuthor}>Skriver: {item.author}</Text>
               <Text style={styles.chatName}>{item.name}</Text>
-              <Text style={styles.latestMessage}>{parseText(item.latestMessage)}</Text>
+              <Text style={styles.latestMessage}>
+                {parseText(item.latestMessage)}
+              </Text>
             </View>
             <Text style={styles.timestamp}>{item.timestamp}</Text>
           </TouchableOpacity>
